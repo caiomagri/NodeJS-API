7d60bbb2cc30196e930c069bb1d48f678a63bb24
Caio Magri <kaioeduardofm@gmail.com>
2018-11-09 17:13:06 -0200

    [ADD] Classe generica para o crud e novos models

--- common/router.ts
+++ common/router.ts
@@ -17,4 +17,19 @@ export abstract class Router extends EventEmitter {
             return next()
         }
     }
+
+    renderAll(response: restify.Response, next: restify.Next){
+        return (documents: any[]) => {
+            if (documents) {
+                documents.forEach(document => {
+                    this.emit('beforeRender', document)
+                })
+                response.json(document)
+            }else{
+                response.json([])
+            }
+
+            return next()
+        }
+    }
 }
